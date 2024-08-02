828a09fa9ce14a439ffaa80dae184490a5d862b1
Cool <990299103@qq.com>
2024-08-02 22:27:33 +0800

    fix：“修复刷新页面，数据无法同步bug”

--- src/services/user.ts
+++ src/services/user.ts
@@ -1,10 +1,10 @@
 import myAxios from "../plugins/myAxios";
-import {getCurrentUserState, setCurrentUserState} from '../states/user.ts'
+import {setCurrentUserState} from '../states/user.ts'
 export const getCurrentUser = async () => {
-    const currentUser = getCurrentUserState();
-    if (currentUser) {
-        return currentUser;
-    }
+    // const currentUser = getCurrentUserState();
+    // if (currentUser) {
+    //     return currentUser;
+    // }
     // 不存在则从远程获取
     const res = await myAxios.get('/user/current');
     if (res.code === 0) {
