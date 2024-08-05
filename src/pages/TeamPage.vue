017933b355db81f7396039849841b02ea2779110
Cool <990299103@qq.com>
2024-08-05 09:13:13 +0800

    feat：“搜索队伍功能开发”

--- src/pages/TeamPage.vue
+++ src/pages/TeamPage.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="teamPage">
+    <van-search v-model="searchText" placeholder="搜索队伍" @search="OnSearch"/>
     <van-button type="primary" @click="doJoinTeam">加入队伍</van-button>
     <team-card-list :teamList="teamList"/>
+    <van-empty v-if="teamList?.length<1" description="数据为空"/>
   </div>
 </template>
 
@@ -20,15 +22,30 @@ const doJoinTeam = () => {
   })
 }
 const teamList = ref([]);
-onMounted(async () => {
-  const res = await myAxios.get("/team/list");
-  console.log("res：",res)
-  if (res.code === 0) {
+onMounted( () => {
+  listTeam();
+})
+const OnSearch =(val)=>{
+  listTeam(val);
+}
+/**
+ * 搜索队伍
+ * @param val
+ * @returns {Promise<void>}
+ */
+const listTeam = async (val = '') => {
+  const res = await myAxios.get("/team/list", {
+    params: {
+      searchText: val,
+      pageNum: 1,
+    },
+  });
+  if (res?.code === 0) {
     teamList.value = res.data;
   } else {
-    showFailToast("加载队伍失败，请刷新")
+    showFailToast('加载队伍失败，请刷新重试');
   }
-})
+}
 </script>
 
 <style scoped>
